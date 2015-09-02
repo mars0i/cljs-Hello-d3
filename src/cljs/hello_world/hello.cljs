d548b8fccb254feb38547941357fb376697b0f59
mars0i <mabrams@uab.edu>
2015-09-01 22:57:33 -0500

    moved clj->js conversion of data seq to separate binding

--- src/cljs/hello_world/hello.cljs
+++ src/cljs/hello_world/hello.cljs
@@ -1,4 +1,10 @@
-;; Clojurescripot Hello World with basic d3.js examples
+;;; Clojurescript Hello World with basic d3.js examples
+
+;;; This software is copyright 2015 by Marshall Abrams, and
+;;; is distributed under the Gnu General Public License version 3.0 as
+;;; specified in the file LICENSE, except for code that comes from
+;;; other authors (such as some comments by Mike Bostock in the d3update 
+;;; function).
 
 (ns hello-world.hello
   (:require [clojure.string :as str]))
@@ -7,14 +13,16 @@
 (def height 500)
 
 ;;;;;;;;;;;;;;;;;;;;;;
-;; Some html: the "Hello World!"
+;; EXAMPLE 0
+;; Some html: the "Hello World!" statement.
 (.write js/document "<center><h1>Whatever, daddyo!</h1></center>")
 
 ;;;;;;;;;;;;;;;;;;;;;;
+;; EXAMPLE 1
 ;; Three overlapping circles, based on example in the strokes repo:
 ;; https://github.com/dribnet/strokes/tree/master/examples/venn-simple
 
-;; Note clj->js turns a Clojure map into a Javascript Object.
+;; NOTE clj->js turns a Clojure map into a Javascript Object.
 ;; Passing an Object to d3's attr is allowed as a way of setting
 ;; multiple attributes in one call to attr.
 
@@ -34,18 +42,18 @@
     (.attr (clj->js {:cx 450 :cy 300 :r 200 :class "left"})))
 
 ;;;;;;;;;;;;;;;;;;
+;; EXAMPLE 2
 ;; Translation with minor modifications of Bostock's general update pattern I 
 ;; example: http://bl.ocks.org/mbostock/3808218
 ;; Some of the comments are copied directly from Bostock's code.
-;; (With help from consultation of the version in the strokes 
-;; repo, 
+;; (With help from consultation of the version in the strokes repo, 
 ;; https://github.com/dribnet/strokes/blob/master/examples/general-update-pattern/src/gup1/gup1.cljs
 ;; which was similar to my original attempt.)
 
 ;(declare d3update)
-; If d3update defn is not before its use, this allows it compile, but
-; it won't run in the browser.  Wierdness of clojure->javascript
-; interop.  Not sure why.
+; If d3update defn is not before its use, this would allow it compile, but
+; it won't run in the browser.  Weirdness of clojure->javascript interop.
+;; Not sure why.
 
 (def alphabet (map char (range 97 (+ 97 26)))) ; lowercase letters a through z
 
@@ -56,20 +64,22 @@
               (.append "svg")
               (.attr (clj->js {:width width :height height}))
               (.append "g") ; so we can move the letters to a nice place in svg
-              (.attr "transform" (str "translate(32," (/ height 3) ")")))) 
+              (.attr "transform" (str "translate(32," (/ height 4) ")")))) 
+
 
 (defn d3update 
   "Given data in a Clojurescript sequence, converts it to a Javascript array
   and uses it to update the previous d3 display--either by creating something
   where there was nothing, by adding to it, by shortening it, etc."
-  [data]
+  [clj-data]
   ;; Here's one way to print to console.  
   ;(.log js/console (pr-str data)) ;(.log js/console (pr-str (type data)))
   ;; Note Clojurescript doesn't Clojure's class function, but you can use type.
 
   ;; DATA JOIN
   ;; Join new data with old elements, if any.
-  (let [text (-> svg2g (.selectAll "text") (.data (clj->js data)))] ; note conversion
+  (let [data (clj->js clj-data) ; Important: convert Clojure collection to Javascript array.
+        text (-> svg2g (.selectAll "text") (.data data))]
 
     ;; UPDATE
     ;; Update old elements as needed.
@@ -80,9 +90,9 @@
     (-> text 
         (.enter)
         (.append "text")
-        (.attr (clj->js {:class "enter"    ; new elts have a different class than old
-                         :x     #(* %2 32) ; move each element 32px right of preceding
-                         :dy    ".35em"})))
+        (.attr (clj->js {:class "enter"     ; new elts have a different class than old
+                         :x     #(* %2 32)  ; move each element 32px right of preceding
+                         :dy    ".35em"}))) ; see note above about clj->js and maps
 
     ;; ENTER + UPDATE
     ;; Appending to the enter selection expands the update selection to include
@@ -101,7 +111,7 @@
 ;; Function that will be called repeatedly after a delay to change the display.
 (defn intervalfn
   "Selects a random subset of letters from alphabet, alphabetizes the result,
-  and passes it to d3update."
+  and passes it to update."
   []
   (-> alphabet
       shuffle
